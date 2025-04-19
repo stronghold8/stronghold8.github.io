81c43723c64bc31f67e2ccef025ddf5b07157550
nZero <damun902@gmail.com>
2025-04-19 23:42:16 +0900

    ad

--- js/script.js
+++ js/script.js
@@ -267,7 +267,7 @@ function showContainer(contentId)
 /* ============================================== Category-inner-boxes ============================================== */
 //카테고리 클릭 시, (HTML섹션이 로딩 후에) 포스트 아이템들을 박스로 만들어서 미리보기로 show
 
-const categories = ['javascript', 'data-structure', 'ai', 'java', 'network', 'math', 'blog', 'minecraft', 'chinese', 'japanese', 'cpp', 'python', 'security', 'order'];
+const categories = ['javascript', 'data-structure', 'ai', 'java', 'network', 'math', 'blog', 'minecraft', 'chinese', 'japanese', 'cpp', 'python', 'security', 'order', 'os'];
 
 for (let i = 0; i < categories.length; i++){
   const category = categories[i];
@@ -409,6 +409,8 @@ function getCategoryName(element){
       return "Computer Security";
     case "receipt":
       return "Receipt";
+    case "os":
+      return "OS";
     
   }
 }
